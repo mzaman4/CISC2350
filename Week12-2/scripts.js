64bbed707a4587d6eaf1442ff4453ade04f256fd
mzaman4 <31833582+mzaman4@users.noreply.github.com>
2017-11-16 18:02:38 -0500

    Add files via upload

---
+++ Week12-2/scripts.js
@@ -0,0 +1,4 @@
+  function myFunction() {
+    console.log("Bonus revealed");
+     document.getElementById("demo").innerHTML = "Great white sharks swim at an everage of 25 mph. Whale sharks swim at an average of 3 mph. Thresher sharks swim at an average of 30 mph.";
+}
