8bdd3028c15eb3e4762bffcc9a9ae03489dcfbb4
mzaman4 <31833582+mzaman4@users.noreply.github.com>
2017-12-07 04:11:36 -0500

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,6 @@
+  function myFunction() {
+
+    console.log("Reveal PSN");
+    
+    document.getElementById("demo").innerHTML = "Want to face me or play alongside me? Contact me on your PS4 by using my PSN name: DinoAfridi";
+}
