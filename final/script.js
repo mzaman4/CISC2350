4308f745c9e5f29ff7fec9062df0bd06e77ae8a2
mzaman4 <31833582+mzaman4@users.noreply.github.com>
2019-02-27 21:32:01 -0500

    Delete script.js

--- final/script.js
+++
@@ -1,6 +0,0 @@
-  function myFunction() {
-
-    console.log("Reveal PSN");
-    
-    document.getElementById("demo").innerHTML = "Want to face me or play alongside me? Contact me on your PS4 by using my PSN name: DinoAfridi";
-}
